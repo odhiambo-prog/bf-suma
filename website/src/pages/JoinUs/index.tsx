6c68a6fcd2b396ab534874a53fa3ebaf7cbd54d9
BonfaceGabriel <bonfacegabriel44@gmail.com>
2026-06-14 14:00:03 +0300

    bump card width to max-w-5xl

--- website/src/pages/JoinUs/index.tsx
+++ website/src/pages/JoinUs/index.tsx
@@ -156,7 +156,7 @@ export default function JoinUs() {
               autoPlay
               showArrows
               showDots={false}
-              className="max-w-4xl mx-auto"
+              className="max-w-5xl mx-auto"
             />
           </div>
         </section>
